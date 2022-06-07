90348822f1ee31f80f918e6d833a77edab5d0270
Christopher Weems <hello@christopherweems.com>
2022-06-07 11:45:26 -0400

    Rename `Locale.Language` as `Locale.LCLanguage`

--- Sources/lc-locale/Locale+Extensions.swift
+++ Sources/lc-locale/Locale+Extensions.swift
@@ -4,7 +4,11 @@
 @_exported import struct Foundation.Locale
 
 public extension Locale {
-    enum Language: String, CaseIterable {
+    @available(macOS, deprecated, obsoleted: 13, renamed: "LCLanguage")
+    @available(*, deprecated, renamed: "LCLanguage")
+    typealias Language = LCLanguage
+    
+    enum LCLanguage: String, CaseIterable {
         case chinese = "zh"
         case danish = "da"
         case english = "en"
@@ -17,76 +21,76 @@ public extension Locale {
         
     }
     
-    static func australia(language: Language = .english) -> Locale {
+    static func australia(language: LCLanguage = .english) -> Locale {
         .init(identifier: "\(language.rawValue)_AU")
     }
     
-    static func austria(language: Language = .german) -> Locale {
+    static func austria(language: LCLanguage = .german) -> Locale {
         .init(identifier: "\(language.rawValue)_AT")
     }
     
-    static func brazil(language: Language = .portuguese) -> Locale {
+    static func brazil(language: LCLanguage = .portuguese) -> Locale {
         .init(identifier: "\(language.rawValue)_BR")
     }
     
-    static func canada(language: Language) -> Locale {
+    static func canada(language: LCLanguage) -> Locale {
         .init(identifier: "\(language.rawValue)_CA")
     }
     
-    static func chile(language: Language = .spanish) -> Locale {
+    static func chile(language: LCLanguage = .spanish) -> Locale {
         .init(identifier: "\(language.rawValue)_CL")
     }
     
-    static func china(language: Language = .chinese) -> Locale {
+    static func china(language: LCLanguage = .chinese) -> Locale {
         .init(identifier: "\(language.rawValue)_CN")
     }
     
-    static func columbia(language: Language = .spanish) -> Locale {
+    static func columbia(language: LCLanguage = .spanish) -> Locale {
         .init(identifier: "\(language.rawValue)_CO")
     }
     
-    static func cuba(language: Language = .spanish) -> Locale {
+    static func cuba(language: LCLanguage = .spanish) -> Locale {
         .init(identifier: "\(language.rawValue)_CU")
     }
     
-    static func denmark(language: Language = .spanish) -> Locale {
+    static func denmark(language: LCLanguage = .spanish) -> Locale {
         .init(identifier: "\(language.rawValue)_DK")
     }
     
-    static func france(language: Language = .french) -> Locale {
+    static func france(language: LCLanguage = .french) -> Locale {
         .init(identifier: "\(language.rawValue)_FR")
     }
     
-    static func germany(language: Language = .german) -> Locale {
+    static func germany(language: LCLanguage = .german) -> Locale {
         .init(identifier: "\(language.rawValue)_DE")
     }
     
-    static func japan(language: Language = .japanese) -> Locale {
+    static func japan(language: LCLanguage = .japanese) -> Locale {
         .init(identifier: "\(language.rawValue)_JP")
     }
     
-    static func southKorea(language: Language = .korean) -> Locale {
+    static func southKorea(language: LCLanguage = .korean) -> Locale {
         .init(identifier: "\(language.rawValue)_KR")
     }
     
-    static func spain(language: Language = .spanish) -> Locale {
+    static func spain(language: LCLanguage = .spanish) -> Locale {
         .init(identifier: "\(language.rawValue)_ES")
     }
     
-    static func unitedStates(language: Language = .english) -> Locale {
+    static func unitedStates(language: LCLanguage = .english) -> Locale {
         .init(identifier: "\(language.rawValue)_US")
     }
     
-    static func unitedKingdom(language: Language = .english) -> Locale {
+    static func unitedKingdom(language: LCLanguage = .english) -> Locale {
         .init(identifier: "\(language.rawValue)_UK")
     }
     
 }
 
 public extension Locale {
-    var language: Language? {
+    var language: LCLanguage? {
         guard let languageCode = self.languageCode else { return nil }
-        return Language.allCases.first { $0.rawValue == languageCode }
+        return LCLanguage.allCases.first { $0.rawValue == languageCode }
     }
     
 }
